248dee3661b6ee5d31270ec7388ce3791640bb7d
Jiří Šuster <suster.ji@gmail.com>
2024-03-28 18:21:18 +0100

    ukoly 1 a 5

--- otazka1.py
+++ otazka1.py
@@ -1,8 +1,8 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from cleverminer import cleverminer
 
-# Load the dataset
 data1 = pd.read_csv('Crimes_clean_2016.csv')
 
 clm = cleverminer(df=data1, target='Day', proc='CFMiner',
@@ -18,3 +18,22 @@ clm = cleverminer(df=data1, target='Day', proc='CFMiner',
                       })
 
 clm.print_rulelist()
+
+subset = data1[data1['Time Category'].isin(['Afternoon', 'Forenoon']) 
+               & data1['Arrest'].isin(['False']) & data1['Domestic'].isin(['False'])]
+
+# Count crimes for each day of the week
+counts = subset['Day'].value_counts()
+# total of counts
+print(counts.sum()) #prints 80399
+
+days_order = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    
+# Sort counts by the days of the week
+counts = counts.loc[days_order]
+
+counts.plot(kind='bar')
+plt.title("Title")
+plt.xlabel("Day of the Week")
+plt.ylabel("Number of Crimes")
+plt.show()
